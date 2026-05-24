ba1d0495c41edffaf913a9ae718b325b59b27d50
RichardSoup <richardsilvavenuto@gmail.com>
2026-05-24 16:19:06 -0300

    Add FirstSection component and update NavBar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import FirstSection from './components/FirstSection'
 import NavBar from './components/NavBar'
 function App() {
 
   return (
     <div className='h-screen w-screen bg-gray-400'>
       <NavBar />
+      <FirstSection />
     </div>
   )
 }
---
+++ src/components/FirstSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FirstSection() {
+  return (
+    <div>
+        <h1>First Section</h1>
+    </div>
+  )
+}
