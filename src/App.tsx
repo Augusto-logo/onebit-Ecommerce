0fa38bc804be18cd026c14e95e648be68dc6e6c4
Augusto Neto <augusto2.logo@gmail.com>
2026-05-24 14:35:26 -0300

    Fix lorem

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='h-screen w-screen bg-gray-400'>
       <Header />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper lacus augue. Praesent fringilla malesuada turpis, vitae laoreet dolor mollis sit amet. Sed vel enim suscipit, hendrerit nisi commodo, posuere tortor. In accumsan leo ut eleifend iaculis. Morbi pretium tincidunt mauris, id facilisis nisl. Quisque venenatis tellus consectetur, dapibus mi ut, consequat leo. Maecenas ultrices ipsum turpis, sed egestas nisl fermentum sit amet. Morbi ut felis nec risus lobortis ultrices. Nam in urna nec orci scelerisque feugiat id quis nisl. Praesent bibendum nisi vitae urna imperdiet consequat. Curabitur vel est sed dui pharetra maximus. Cras eget iaculis turpis. 
+      Voltar ao normal
     </div>
   )
 }
