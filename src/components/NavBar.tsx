1c7170bde1478739cb2308a9af750143de373401
Augusto Neto <augusto2.logo@gmail.com>
2026-05-25 15:52:38 -0300

    correção barra de navegação

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
-    <div>
-      <nav className="bg-black text-white p-4 flex justify-between">
+    <div className="flex flex-row w-full">
+      <nav className=" bg-black text-white p-4 flex flex-row w-full justify-between ">
         <div>
           <h1 className="text-orange-500 text-2xl">
             RUNMAX
